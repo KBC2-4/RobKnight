cae065b699c004d1e9dd1eae83eda1d856693bdf
erimaki1844 <106945196+erimaki1844@users.noreply.github.com>
2023-11-08 17:16:08 +0900

    スイッチの追加

--- Assets/button.cs
+++ Assets/button.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 public class button : MonoBehaviour
 {
 
-    bool a;
+    bool push_flg;
+    float start_pds_y;
 
     // Start is called before the first frame update
     void Start()
     {
-        a = true;
+        push_flg = false;
+        start_pds_y = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (a = true)
+
+        if (push_flg == true)
         {
-            transform.position += transform.up * Time.deltaTime;
+            transform.position  += -transform.up * Time.deltaTime;
+            if (start_pds_y-0.1 >= transform.position.y)   { push_flg = false; }
         }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //if (collision.Player.Body05) { }
+        push_flg = true;
     }
 }
