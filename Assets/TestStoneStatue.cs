b52f13015484725a5af5b8753420fe8d832be769
yusaku-uema <106945257+yusaku-uema@users.noreply.github.com>
2023-11-14 12:14:08 +0900

    アニメーション関係

--- Assets/TestStoneStatue.cs
+++ Assets/TestStoneStatue.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class TestStoneStatue : MonoBehaviour
 {
+
+
+   void Awake()
+    {
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,18 @@ public class TestStoneStatue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Transform myTransform = this.transform;
+
+        //永遠と回転する処理
+        // transformを取得
+        //// ワールド座標基準で、現在の回転量へ加算する
+        //myTransform.Rotate(0, 1.0f, 0, Space.World);
+
+
+        Vector3 localAngle = myTransform.localEulerAngles;
+        localAngle.z = 15.0f; // ローカル座標を基準に、z軸を軸にした回転を10度に変更
+
+        myTransform.localEulerAngles = localAngle; // 回転角度を設定
+
     }
 }
