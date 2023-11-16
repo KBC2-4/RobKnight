50a823e896a52d8741c61884c1e0cca7f7bfb8c3
arikawaaaaaaaaa <taisei-arikawa-k21h@stu.kbc.ac.jp>
2023-11-16 11:29:05 +0900

    ボスゴブリンの仲間呼び修正
    仲間呼びモーションが2回連続で再生される問題の修正

--- Assets/Scripts/Enemy/Actions/CallGoblin.cs
+++ Assets/Scripts/Enemy/Actions/CallGoblin.cs
@@ -9,36 +9,23 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "EnemyActions/CallGoblin")]
 public class CallGoblin : EnemyAction
 {
-    private bool IsCalled = false;
-    private void OnEnable()
-    {
-        //行動の初期化
-        IsCalled = false;
-    }
 
     public override void Act(EnemyController controller)
     {
         // 現在再生中のアニメーションの状態を取得
         AnimatorStateInfo stateInfo = controller.animator.GetCurrentAnimatorStateInfo(0);
 
-        Debug.Log($"State:{IsCalled}");
-
-        //アクション開始から一定時間が経っていないならIsCalledをリセット
-        if (ActionTime < 60) IsCalled = false;
-
-        //仲間を呼ぶモーションを行ったことを記録
-        if (stateInfo.IsName("Call") && 60 <= ActionTime) IsCalled = true;
-
-        if (!IsCalled)
-        {
-            //仲間を呼ぶモーションのトリガーをセット
-            controller.animator.SetTrigger("CallTrigger");
-        }
-        else if(stateInfo.IsName("Idle"))
+        if (stateInfo.IsName("EndCall")) 
         {
             //モーションが終了してアニメーターがIdle状態になれば行動終了
             IsComplete = true;
+            controller.animator.ResetTrigger("CallTrigger");
             return;
+        }
+        else if (stateInfo.IsName("Idle") && !IsComplete)
+        {
+            //仲間を呼ぶモーションのトリガーをセット
+            controller.animator.SetTrigger("CallTrigger");
         }
     }
 }
