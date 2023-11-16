5bc32dae39f2fe8c8d8b20c4f71405abd5800297
erimaki1844 <106945196+erimaki1844@users.noreply.github.com>
2023-11-16 18:27:56 +0900

    test

--- Assets/Stone_Statue_Move.cs
+++ Assets/Stone_Statue_Move.cs
@@ -5,29 +5,37 @@ using UnityEngine;
 public class Stone_Statue_Move : MonoBehaviour
 {
 
-	public float pushPower = 2.0f;
+    public float pushPower;
+    public bool push_flg;
+    public Vector3 pushDir;
+    private Rigidbody rb;
 
-	// Start is called before the first frame update
-	void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        PlayerController playerController;
-        GameObject obj = GameObject.Find("MaleCharacterPolyart");
-        playerController = obj.GetComponent<PlayerController>();
+        push_flg = false;
+        pushPower = 2.0f;
+        rb = this.GetComponent<Rigidbody>();  // rigidbody‚ðŽæ“¾
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        if (push_flg == true)
+        {
+            transform.position += -transform.up * Time.deltaTime;
+            push_flg = false;
+        }
     }
 
-	void OnCollisionEnter(Collision collision)
-	{
+    void OnTriggerStay(Collider other)
+    {
+        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            pushDir = new Vector3(10 * Time.deltaTime, 0,10 * Time.deltaTime);
 
-		Vector3 pushDir = new Vector3(collision.transform.position.x, 0, collision.transform.position.z);
-
-        GetComponent<Rigidbody>().velocity = pushDir * pushPower;
-	}
-
-   
+            push_flg = true;
+        }
+    }
 }
