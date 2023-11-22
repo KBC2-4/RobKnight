76dabafa9b55683f9f67cb08441f22932e9dd3f2
Yuta26462 <yuuta-miyagi-k21h@stu.kbc.ac.jp>
2023-11-22 09:03:52 +0900

    ミニマップの表示を変更

--- Assets/Scripts/UI/MiniMapIconTracker.cs
+++ Assets/Scripts/UI/MiniMapIconTracker.cs
@@ -1,29 +1,62 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MiniMapIconTracker : MonoBehaviour
 {
-    public Transform target; // ゲームワールドの対象
-    public RectTransform minimapRectTransform; // ミニマップのRectTransform
-    public Vector2 worldSize = new Vector2(1000, 1000); // ワールドのサイズ
-    public RectTransform icon; // ミニマップ上のアイコン
+    public Camera minimapCamera; // ミニマップのカメラ
+    public RectTransform minimapRect; // ミニマップUIのRectTransform
+    public GameObject enemyIconPrefab; // エネミーアイコンのプレハブ
+    public GameObject interactiveIconPrefab; // インタラクティブアイコンのプレハブ
+
+    private List<GameObject> enemyIcons = new List<GameObject>(); // アクティブなエネミーアイコンのリスト
+    private List<GameObject> interactiveIcons = new List<GameObject>(); // アクティブなインタラクティブアイコンのリスト
 
     void Update()
     {
-        // Vector2 minimapSize = new Vector2(minimapRectTransform.rect.width, minimapRectTransform.rect.height);
-        // Vector2 minimapPosition = WorldToMinimapPosition(target.position, minimapSize);
-        // minimapRectTransform.anchoredPosition = minimapPosition;
-        // icon.anchoredPosition = minimapPosition;
+        UpdateIcons(GameObject.FindGameObjectsWithTag("Enemy"), enemyIcons, enemyIconPrefab);
+        UpdateIcons(GameObject.FindGameObjectsWithTag("Interactable"), interactiveIcons, interactiveIconPrefab);
     }
 
-    Vector2 WorldToMinimapPosition(Vector3 worldPosition, Vector2 minimapSize)
+    void UpdateIcons(GameObject[] objects, List<GameObject> icons, GameObject prefab)
     {
-        Vector2 normalizedPosition = new Vector2(
-            (worldPosition.x + worldSize.x / 2) / worldSize.x,
-            (worldPosition.z + worldSize.y / 2) / worldSize.y
-        );
-        return new Vector2(
-            (normalizedPosition.x - 0.5f) * minimapSize.x,
-            (normalizedPosition.y - 0.5f) * minimapSize.y
-        );
+        // アイコンをオブジェクトの数に合わせて生成
+        while (icons.Count < objects.Length)
+        {
+            // 新しいアイコンを生成
+            GameObject icon = Instantiate(prefab, minimapRect);
+            icon.SetActive(true);
+            icons.Add(icon);
+            
+            // 生成し、iconsに追加
+            icons.Add(Instantiate(prefab, minimapRect));
+        }
+        
+        // アイコンをオブジェクトの数に合わせて削除
+        while (icons.Count > objects.Length)
+        {
+            Destroy(icons[icons.Count - 1]);
+            icons.RemoveAt(icons.Count - 1);
+        }
+
+        // アイコンの位置を更新
+        for (int i = 0; i < objects.Length; i++)
+        {
+            // ミニマップカメラのビューポート座標に変換
+            Vector3 minimapPosition = minimapCamera.WorldToViewportPoint(objects[i].transform.position);
+            // アイコンがミニマップの範囲内にあるか確認
+            if (minimapPosition.x >= 0 && minimapPosition.x <= 1 && minimapPosition.y >= 0 && minimapPosition.y <= 1)
+            {
+                // ビューポート座標をUIのアンカー座標に変換
+                minimapPosition.x = (minimapPosition.x * minimapRect.sizeDelta.x) - (minimapRect.sizeDelta.x * 0.5f);
+                minimapPosition.y = (minimapPosition.y * minimapRect.sizeDelta.y) - (minimapRect.sizeDelta.y * 0.5f);
+                icons[i].GetComponent<RectTransform>().anchoredPosition = minimapPosition;
+                icons[i].SetActive(true);
+            }
+            else
+            {
+                // マップの範囲外の場合はアイコンを非表示にする
+                icons[i].SetActive(false);
+            }
+        }
     }
 }
