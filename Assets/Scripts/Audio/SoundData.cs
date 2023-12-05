2947aca8f3c176baf7174bc81253adb9ebe3f494
Yuta26462 <yuuta-miyagi-k21h@stu.kbc.ac.jp>
2023-12-06 06:51:09 +0900

    AudioManagerの負荷軽減化

--- Assets/Scripts/Audio/SoundData.cs
+++ Assets/Scripts/Audio/SoundData.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 public class SoundData
 {
 
-    [SerializeField] private string soundType; //音声の種類
+    // [SerializeField] private string soundType; //音声の種類
     //soundTypeのGetter
-    public string SoundType => soundType;
+    // public string SoundType => soundType;
+    
+    [SerializeField] private string fileName; // 音声ファイルの名前
+    public string FileName => fileName;
+    
     [SerializeField] private AudioClip audioClip;   //音声データ
     //audioClipのGetter
     public AudioClip AudioClip => audioClip;
-    [Range(0, 1)]
-    [SerializeField] private float volume = 1.0f;   //音量
+    [SerializeField, Range(0, 1)] private float volume = 1.0f;   //音量
 }
