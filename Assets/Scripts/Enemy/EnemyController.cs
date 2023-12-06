f451c6c1d329c9f7c2f41ded9e9bfad6f2b84d89
arikawaaaaaaaaa <taisei-arikawa-k21h@stu.kbc.ac.jp>
2023-12-06 14:27:40 +0900

    ボスゴブリンの憑依後の挙動を修正など
    ボスゴブリンに憑依した後アニメーションが再生されない問題を修正
    ゲームをリセットした時にボスゴブリンの行動が初期化されない問題を修正

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -138,6 +138,11 @@ public class EnemyController : MonoBehaviour
         animator.ResetTrigger("AttackTrigger");
         animator.ResetTrigger("RushTrigger");
         animator.ResetTrigger("CallTrigger");
+
+        animator.SetBool("IsPossession", true);
+
+        EndAttack();
+        DisableHit();
         //Animation lightEffectAnimation = lightEffect.GetComponent<Animation>();
         //if (lightEffectAnimation != null)
         //{
@@ -157,12 +162,12 @@ public class EnemyController : MonoBehaviour
 
         gameObject.GetComponent<Rigidbody>().isKinematic = true;
         gameObject.GetComponent<CapsuleCollider>().isTrigger = true;
+        gameObject.GetComponent<BoxCollider>().isTrigger = true;
         bool isPossession = player.gameObject.GetComponent<PlayerController>().isPossession;
 
         // ボスゴブリンまたはプレイヤーが憑依しているエネルギー以外は10秒後にオブジェクトを破棄する
         if (enemyData.enemyName != "BossGoblin" && isPossession)
         {
-            
             // 10秒後にオブジェクトを破棄する。
             Destroy(gameObject, 10.0f);
         }
