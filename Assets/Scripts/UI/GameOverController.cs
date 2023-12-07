3c84f35ed88a0cc6bf741f070ce709b47be48180
Yuta26462 <yuuta-miyagi-k21h@stu.kbc.ac.jp>
2023-12-07 16:17:41 +0900

    ゲームオーバー画面にSEの実装

--- Assets/Scripts/UI/GameOverController.cs
+++ Assets/Scripts/UI/GameOverController.cs
@@ -10,6 +10,9 @@ public class GameOverController : MonoBehaviour
     //　最初にフォーカスするゲームオブジェクト
     [SerializeField]
     public GameObject firstSelect;
+    [SerializeField] private AudioSource bgmAudioSource; // 音量を小さくするBGM再生に使用するオーディオソース
+    [SerializeField] private AudioSource seAudioSource; // ループさせない為SE再生に使用するオーディオソース
+    [SerializeField] private AudioClip bgm; // 再生するBGM
 
 
     private void Start()
@@ -21,9 +24,13 @@ public class GameOverController : MonoBehaviour
     {
         // ゲームの時間を停止
         Time.timeScale = 0f;
-        Debug.Log("ゲームオーバー！");
         gameOverPanel.SetActive(true);
         EventSystem.current.SetSelectedGameObject(firstSelect);
+        // 音量を小さくする
+        bgmAudioSource.volume = 0.5f;
+        seAudioSource.volume = 0.7f;
+        // SE再生
+        seAudioSource.PlayOneShot(bgm);
     }
 
     public void RestartGame()
@@ -38,6 +45,13 @@ public class GameOverController : MonoBehaviour
         // ゲームの時間を初期状態で通常の速度に設定
         Time.timeScale = 1f;
         SceneManager.LoadScene("Title");
+    }
+
+    private void OnDestroy()
+    {
+        bgmAudioSource.volume = 1f;
+        seAudioSource.volume = 1f;
+        seAudioSource.Stop();
     }
 }
 
