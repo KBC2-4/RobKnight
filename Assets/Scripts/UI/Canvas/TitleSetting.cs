1b6e78cefac0d4218ea2c1bbb260164fc70696c3
Yuta26462 <yuuta-miyagi-k21h@stu.kbc.ac.jp>
2023-12-11 11:25:32 +0900

    タイトル画面から設定画面に推移できない不具合の修正

---
+++ Assets/Scripts/UI/Canvas/TitleSetting.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TitleSetting : MonoBehaviour
+{
+    [SerializeField] private GameObject _settingsPanel;
+    private GameObject lastSelectedButton; // 最後に選択されたボタン
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 設定画面を非アクティブに設定
+        _settingsPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // <summary>
+    // 「設定」ボタンから呼び出される 設定画面を開く
+    // </summary>
+    public void ShowSettingsPanel()
+    {
+        // ポーズ前の選択を保存
+        lastSelectedButton = EventSystem.current.currentSelectedGameObject;
+        _settingsPanel.SetActive(true);
+    }
+
+    // <summary>
+    // 「戻る」ボタンから呼び出される 設定画面を閉じる
+    // </summary>
+    public void HideSettingsPanel()
+    {
+        _settingsPanel.SetActive(false);
+        // SEが再生されないようにする
+        UISoundManager.Instance.SetProgrammaticSelect();
+        // ポーズ前の選択に戻す
+        EventSystem.current.SetSelectedGameObject(lastSelectedButton);
+    }
+}
