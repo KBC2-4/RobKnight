38a56a556dbc47ccf3c136db12794c997e726948
arikawaaaaaaaaa <taisei-arikawa-k21h@stu.kbc.ac.jp>
2023-12-11 10:49:46 +0900

    追加：ボスがエリア外に吹き飛んだ時に初期地点に戻る

--- Assets/closure.cs
+++ Assets/closure.cs
@@ -14,13 +14,33 @@ public class closure : MonoBehaviour
     private bool isActivated = false;
     private GameObject colliderObject;
 
+    private Vector3 spawnpos = Vector3.zero;
+
+    private void Start()
+    {
+        spawnpos = guard.transform.position;
+    }
+
     // Update is called once per frame
-    private void FixedUpdate()
+    private void Update()
     {
         if (guard.isDeath)
         {
             //紐づけられた敵が倒されたとき、このオブジェクトを削除する
             Destroy(this.gameObject);
+        }
+
+        Vector3 AreaPos = transform.position;
+        Vector3 EnemyPos = guard.transform.position;
+        float AreaX = colliderSize.x / 2 + 3;
+        float AreaY = colliderSize.y / 2 + 3;
+        float AreaZ = colliderSize.z / 2 + 3;
+
+        if ((AreaPos.x + AreaX < EnemyPos.x || EnemyPos.x < AreaPos.x - AreaX) ||
+            (AreaPos.y + AreaY < EnemyPos.y || EnemyPos.y < AreaPos.y - AreaY) ||
+            (AreaPos.z + AreaZ < EnemyPos.z || EnemyPos.z < AreaPos.z - AreaZ)) 
+        {
+            guard.transform.position = spawnpos;
         }
     }
     private void OnTriggerEnter(Collider c)
