5f2113a484265b500712e56c70a496a4f957dfa8
Yuta26462 <yuuta-miyagi-k21h@stu.kbc.ac.jp>
2023-12-11 09:33:10 +0900

    UI管理クラスを追加し、UIバグ修正

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance { get; private set; }
+
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject settingsMenu;
+
+    private UIState currentState;  // 現在のステート
+    private UIState previousState; // 前回のステート
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public enum UIState
+    {
+        Gameplay,       // ゲームプレイ画面
+        PauseMenu,      // ポーズ画面
+        SettingsMenu    // 設定画面
+    }
+
+    public void ChangeState(UIState newState)
+    {
+        if (currentState != newState)
+        {
+            // 現在のステートを前回のステートとして保存
+            previousState = currentState;
+            currentState = newState;
+            // UpdateUIState();
+        }
+    }
+
+    private void UpdateUIState()
+    {
+        switch (currentState)
+        {
+            case UIState.Gameplay:
+                pauseMenu.SetActive(false);
+                settingsMenu.SetActive(false);
+                break;
+            case UIState.PauseMenu:
+                pauseMenu.SetActive(true);
+                break;
+            case UIState.SettingsMenu:
+                settingsMenu.SetActive(true);
+                break;
+        }
+    }
+
+    public UIState GetCurrentState()
+    {
+        return currentState;
+    }
+
+    public void ReturnToPreviousState()
+    {
+        // 前回のステートに戻す
+        ChangeState(previousState);
+    }
+}
